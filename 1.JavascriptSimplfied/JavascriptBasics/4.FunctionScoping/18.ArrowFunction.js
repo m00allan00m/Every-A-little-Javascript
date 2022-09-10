1948ded0020af50a8b7c93a4371305757f12e373
Allan <m00allan00m@gmail.com>
2022-09-11 08:01:20 +0930

    more learning

--- 1.JavascriptSimplfied/JavascriptBasics/4.FunctionScoping/18.ArrowFunction.js
+++ 1.JavascriptSimplfied/JavascriptBasics/4.FunctionScoping/18.ArrowFunction.js
@@ -1,16 +1,40 @@
-printName = (name) => console.log(name)
-printName("Hello")
+// printName = (name) => console.log(name)
+// printName("Hello")
 
-let printName2 = (name, age) => console.log(name)
-printName2("Allan", 6)
+// let printName2 = (name, age) => console.log(name)
+// printName2("Allan", 6)
 
-let printHi = (name) => `Hi ${name}`
+// let printHi = (name) => `Hi ${name}`
 
-console.log(printHi("Allan"))
+// console.log(printHi("Allan"))
 
-function func(x, cb) {
-  cb(x)
-}
+// function func(x, cb) {
+//   cb(x)
+// }
 
-func(10, (variable) => console.log("Hello World"))
- 
+// func(10, (variable) => console.log("Hello World"))
+
+// All things javascript https://www.youtube.com/watch?v=dAAVtx-YW44
+//Arrow functions and Lexical this binding
+//arrow functions are good for callbacks and nested function
+//because they carry on this from their closures.
+//Not goof for constructor, methods(inheritance) , dynamic context
+
+//BOOK: Javascript For Impatient Programmers
+//Arrow function expressions are used especially as arguments of function callbacks
+// and method calls:
+// import * as assert from "assert/strict"
+
+// const add2 = (a, b) => {
+//   return a + b
+// }
+// assert.equal(add2(5, 2), 7)
+// //here begins my rabbit hole what is assert? First encounter with this method/function
+
+// console.log(add2(1, 2))
+// const add3 = (a, b) => a + b
+
+// var assert1 = require('assert');
+// assert1(3 > 7);
+
+
--- 1.JavascriptSimplfied/JavascriptBasics/4.FunctionScoping/22.Closures.js
+++ 1.JavascriptSimplfied/JavascriptBasics/4.FunctionScoping/22.Closures.js
@@ -15,21 +15,66 @@ let b = "Happy" //prints the latest value assignment
 print()
 
 
- */
+//  */
 
-function print(variable) {
-  let c = 3
-  return function print2(variable2) {
-    let d = 4
-    console.log(variable) //1
-    console.log(variable2) //2
-    console.log(c)
-    console.log(d)
-    // console.log(e) //Cannot access
-  }
+// function print(variable) {
+//   let c = 3
+//   return function print2(variable2) {
+//     let d = 4
+//     console.log(variable) //1
+//     console.log(variable2) //2
+//     console.log(c)
+//     console.log(d)
+//     // console.log(e) //Cannot access
+//   }
+// }
+// let a = print(1)
+
+// a(2)
+
+// // let e = 5
+
+// const outer = () => {
+//   const outerVar = "Hello"
+//   const inner = () => {
+//     const innerVar = "Hi"
+//     console.log(outerVar, innerVar)
+//   }
+//   inner()
+//   return inner()
+// }
+
+// const innerFn = outer()
+// innerFn()
+
+// // // inner() undefined! inner() is not in the global context
+// //inner() is in the scope of outer function
+
+// function init() {
+//   const hobby = "Learning Javascript" //in outer block    scope : this is available
+//   const displayHobby = () => {
+//     //inner function
+//     console.log(hobby) //
+//   }
+//   displayHobby() //calling inner function from the outer function
+// }
+
+// init()
+
+//https://www.youtube.com/watch?v=vKJpN5FAeF4
+// Closures Explained in 100 Seconds // Tricky JavaScript Interview Prep
+
+// function outer() {
+//   let state = "🐇"
+//   function inner() {
+//     return `Hello ${state}`
+//   }
+//   return inner
+// }
+
+const months = ["Jan", "Feb", "March", "April", "June", "July"]
+months.push("August")
+
+for (let i = 0; i < months.length; i++) {
+  console.log(months[i])
 }
-let a = print(1)
-
-a(2)
-
-// let e = 5
