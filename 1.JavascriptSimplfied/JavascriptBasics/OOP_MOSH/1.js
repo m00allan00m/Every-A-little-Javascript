5edc20005b0e0f353dee70720b253f12d1109acf
Allan <m00allan00m@gmail.com>
2022-09-12 16:28:47 +0930

    Update 1.js

--- 1.JavascriptSimplfied/JavascriptBasics/OOP_MOSH/1.js
+++ 1.JavascriptSimplfied/JavascriptBasics/OOP_MOSH/1.js
@@ -15,21 +15,21 @@
 // console.log(`Factories and Constructor`)
 // console.log(`This is a Factory Function`)
 
-function FactoriesFunction() {
-  return {
-    radius: 1,
-    draw: function () {
-      return `draw`
-    },
-  }
-}
+// function FactoriesFunction() {
+//   return {
+//     radius: 1,
+//     draw: function () {
+//       return `draw`
+//     },
+//   }
+// }
 
-const recieveFactoriesFunction = new FactoriesFunction()
-console.log(recieveFactoriesFunction)
-const factorise = recieveFactoriesFunction.draw()
-console.log(recieveFactoriesFunction.radius)
-console.log(factorise)
-// console.log(`This is a constructor function`);
+// const recieveFactoriesFunction = FactoriesFunction()
+// console.log(recieveFactoriesFunction)
+// const factorise = recieveFactoriesFunction.draw()
+// console.log(recieveFactoriesFunction.radius)
+// console.log(factorise)
+// // console.log(`This is a constructor function`);
 
 // function ConstructorFunction(radius) {
 //   this.radius = 1
@@ -50,3 +50,58 @@ console.log(factorise)
 // console.log(`Working Properties`)
 // console.log(`Private Properties`);
 // console.log(`Getters/Setters`);
+
+// let x = {}
+
+function Constructed(radius) {
+  this.radius = 1
+  this.draw = function () {
+    console.log(`draw`)
+  }
+}
+
+let construct = new Constructed()
+
+// console.log(`Example Javascript Object Constructor`)
+// console.log(`Good practise to uppercase the name`)
+
+// function personConstructor(firstname, lastname, age, phone, email) {
+//   this.firstname = firstname
+//   this.lastname = lastname
+//   this.age = age
+//   this.phone = phone
+//   this.email = email
+// }
+
+// const Circle = new Function(
+//   "radius",
+//   `
+//   this.radius = radius;
+//   this.draw = function() {
+//     console.log("draw");
+//   }
+// `
+// )
+
+// const newCircle = new Circle(1)
+
+function Circle(radius) {
+  this.radius = radius
+  this.draw = function () {}
+  console.log(`draw`)
+}
+
+const circle = new Circle(10)
+
+
+
+circle.location = {x: 1}
+circle["location"] = {x: 2}
+
+
+const propertyName = "location" //this is a variable
+circle[propertyName] = {x: 300} //this variable is being used
+
+console.log(circle['location']);
+
+
