34f9f69d1f67a3616000de7fa7800be06c6ef1d3
Allan <m00allan00m@gmail.com>
2022-09-17 19:22:36 +0930

    retesting myself > reinforce learning

--- 1.JavascriptSimplfied/JavascriptBasics/DOM/webDEV/domTraversal.js
+++ 1.JavascriptSimplfied/JavascriptBasics/DOM/webDEV/domTraversal.js
@@ -6,3 +6,19 @@
 //Q 6: Target child last using ElementSibling >  Change TEXT > Derrick Rose
 //Q 7: Target grandparent using child 3 using closest
 //Completed
+
+const grandparent = document.querySelector('#grandparent')
+grandparent.style.color = 'red'
+
+const parent1 = grandparent.children[0]
+parent1.style.color = 'red'
+const parent2 = grandparent.children[1]
+parent2.style.color = 'green'
+
+const child2 = parent1.children[1]
+const child1 = child2.previousElementSibling
+const child3 = child2.nextElementSibling
+
+child1.style.color = 'purple'
+child2.style.color = 'blue'
+child3.style.color = 'yellow'
--- 1.JavascriptSimplfied/JavascriptBasics/DOM/webDEV/simpleToDoList.js
+++ 1.JavascriptSimplfied/JavascriptBasics/DOM/webDEV/simpleToDoList.js
@@ -1,7 +1,14 @@
-//select all element
+const list = document.querySelector('#list')
+const form = document.querySelector('#new-item-form')
+const input = document.querySelector('#item-input')
 
-// disable default behaviour that refreshes the page
-      //1.Create a new item
-      //2.Add Item to a list
-      //3.Clear Input
-      //4.Setup event listener to delete
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const item = document.createElement('div')
+  item.classList = 'list-items'
+  list.appendChild(item)
+  item.innerText = input.value
+  item.addEventListener('click', (e) => {
+    item.remove()
+  })
+})
